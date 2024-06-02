4a58373e5efd533ba5b9c5093f0c2c1508bcfb06
Chetan0211 <saichetanreddy0211@gmail.com>
2024-06-02 09:05:31 +0530

    Add pg as active storage

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_05_102231) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,15 +25,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_05_102231) do
     t.integer "price"
     t.integer "available_quantity"
     t.integer "total_quantity"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "borrowers", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "book_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
     t.string "status", default: "requested"
     t.text "comments"
     t.datetime "created_at", null: false
